2faaf2b4c639c9bad169a0ac1cb4d12cddda2de9
edcorrea <edm.correa@hotmail.com>
2023-06-19 11:19:23 -0300

    adding POO - class attribute topic

---
+++ POO/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace ClassesObjetos
+{
+  class Pessoa
+{
+  public string name;
+  public string lastname;
+  public int year;
+}
+}
---
+++ POO/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ClassesObjetos
+{
+   class Program
+ {
+  static void Main(string[] args)
+  {
+    Pessoa p1 = new Pessoa();
+    p1.name = "Edmilson";
+    p1.lastname = "Pinto";
+    p1.year = 25;
+
+    Console.WriteLine("Pessoa 1: " + p1.name + " " + p1.lastname);
+  }
+ }
+}
